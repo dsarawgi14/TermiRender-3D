957d2741b284697d3c9ffabb637b71ae0c775c41
Dhruva <s.dhruva@iitg.ac.in>
2023-12-15 02:11:00 +0530

    reseting rmd

--- rayMarchingDonut.cpp
+++ rayMarchingDonut.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 void stepFunc(double increment, double currectTime, ObjectCollection& collection) {
-    collection.cube["Cube 1"].rotX(4 * sin(currectTime) * increment);
-    collection.cube["Cube 1"].rotY(2 * cos(currectTime) * increment);
-    collection.cube["Cube 1"].setPos(vec3(-2*sin(currectTime*1.2), 0, 0));
+    collection.donut["Donut 1"].rotX(4 * sin(currectTime) * increment);
+    collection.donut["Donut 1"].rotY(2 * cos(currectTime) * increment);
+    collection.donut["Donut 1"].setPos(vec3(-2*sin(currectTime*1.2), 0, 0));
     collection.sphere["Sphere 1"].setPos(vec3(2*sin(currectTime*1.2), 0, 0));
 }
 
@@ -20,16 +20,19 @@ int main() {
 
     collection.sphere["Sphere 1"];
     collection.sphere["Sphere 1"].setRadius(0.7);
-    collection.cube["Cube 1"];
+    collection.donut["Donut 1"];
+    collection.donut["Donut 2"];
 
-    SDFMixer SphereNDonut(&collection.cube["Cube 1"], &collection.sphere["Sphere 1"]);
-    SphereNDonut.setSmoothDifference();
+    SDFMixer SphereNDonut(&collection.donut["Donut 1"], &collection.sphere["Sphere 1"]);
+    SphereNDonut.setSmoothUnion();
+    SDFMixer SphereNDonut2(&collection.donut["Donut 2"], &SphereNDonut);
+    SphereNDonut2.setSmoothUnion();
 
     Scene scene(&SphereNDonut, collection);
     scene.setStepFunction(stepFunc);
     // scene.setMaxFrameRate(100);
     scene.clear();
-    // scene.startFrameTimer();
+    scene.startFrameTimer();
     while(1) {
         scene.render();
     }
